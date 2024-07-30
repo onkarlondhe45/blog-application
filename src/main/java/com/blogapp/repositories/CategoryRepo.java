75fc6cf82c77f41ada3aba37f9a0822c72774cf4
onkarlondhe1 <onkarlondhe@outlook.com>
2024-07-30 10:05:58 +0530

    #modify some changes

--- src/main/java/com/blogapp/repositories/CategoryRepo.java
+++
@@ -1,9 +0,0 @@
-package com.blogapp.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.blogapp.entities.Category;
-
-public interface CategoryRepo extends JpaRepository<Category, Integer>{
-
-}
